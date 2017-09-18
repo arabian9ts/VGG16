193f4a71d8847334d336c866c3d1318ba6a6a052
arabian9ts <nok14.tm59@gmail.com>
2017-09-18 23:38:02 +0900

    append: comment blocks about structure

--- vgg16_structure.py
+++ vgg16_structure.py
@@ -7,6 +7,12 @@ author: arabian9ts
 
 # structure of convolution and pooling layer up to fully-connection layer
 structure = {
+    """
+    size-format:
+                 [ [      convolution_kernel     ], [   bias   ] ]
+                 [ [ f_h, f_w, in_size, out_size ], [ out_size ] ]
+    """
+
     # convolution layer 1
     'conv1_1': [[3, 3, 3, 64], [64]],
     'conv1_2': [[3, 3, 64, 64], [64]],
@@ -40,6 +46,11 @@ structure = {
     'fc8': [[1000, 0, 0, 0], [1000]],
 }
 
+# kernel_size is constant, so defined here
 ksize = [1, 2, 2, 1,]
+
+# convolution-layer-strides is already below
 conv_strides = [1, 1, 1, 1,]
+
+# pooling-layer-strides is already below
 pool_strides = [1, 2, 2, 1,]
