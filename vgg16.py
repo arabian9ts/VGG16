d58afc72414f3757339b096e2300203c48343bf8
arabian9ts <nok14.tm59@gmail.com>
2017-09-18 22:59:16 +0900

    fix: definition of model-structure i.e. list -> dict

--- vgg16.py
+++ vgg16.py
@@ -21,36 +21,32 @@ class Vgg16:
         build() assembles vgg16 network
         """
 
-        # layer_number
-        self.lcnt = 0
+        self.conv1_1 = self.convolution(input, 'conv1_1')
+        self.conv1_2 = self.convolution(self.conv1_1, 'conv1_2')
+        self.pool1 = self.pooling(self.conv1_2, 'pool1')
 
-        self.conv1_1 = self.convolution(input)
-        self.conv1_2 = self.convolution(self.conv1_1)
-        self.pool1 = self.pooling(self.conv1_2)
+        self.conv2_1 = self.convolution(self.pool1, 'conv2_1')
+        self.conv2_2 = self.convolution(self.conv2_1, 'conv2_2')
+        self.pool2 = self.pooling(self.conv2_2, 'pool2')
 
-        self.conv2_1 = self.convolution(self.pool1)
-        self.conv2_2 = self.convolution(self.conv2_1)
-        self.pool2 = self.pooling(self.conv2_2)
+        self.conv3_1 = self.convolution(self.pool2, 'conv3_1')
+        self.conv3_2 = self.convolution(self.conv3_1, 'conv3_2')
+        self.conv3_3 = self.convolution(self.conv3_2, 'conv3_3')
+        self.pool3 = self.pooling(self.conv3_3, 'pool3')
 
-        self.conv3_1 = self.convolution(self.pool2)
-        self.conv3_2 = self.convolution(self.conv3_1)
-        self.conv3_3 = self.convolution(self.conv3_2)
-        self.pool3 = self.pooling(self.conv3_3)
+        self.conv4_1 = self.convolution(self.pool3, 'conv4_1')
+        self.conv4_2 = self.convolution(self.conv4_1, 'conv4_2')
+        self.conv4_3 = self.convolution(self.conv4_2, 'conv4_3')
+        self.pool4 = self.pooling(self.conv4_3, 'pool4')
 
-        self.conv4_1 = self.convolution(self.pool3)
-        self.conv4_2 = self.convolution(self.conv4_1)
-        self.conv4_3 = self.convolution(self.conv4_2)
-        self.pool4 = self.pooling(self.conv4_3)
+        self.conv5_1 = self.convolution(self.pool4, 'conv5_1')
+        self.conv5_2 = self.convolution(self.conv5_1, 'conv5_2')
+        self.conv5_3 = self.convolution(self.conv5_2, 'conv5_3')
+        self.pool5 = self.pooling(self.conv5_3, 'pool5')
 
-        self.conv5_1 = self.convolution(self.pool4)
-        self.conv5_2 = self.convolution(self.conv5_1)
-        self.conv5_3 = self.convolution(self.conv5_2)
-        self.pool5 = self.pooling(self.conv5_3)
-
-        self.fc6 = self.fully_connection(self.pool5, activation=Activation.relu)
-        self.fc7 = self.fully_connection(self.fc6, activation=Activation.relu)
-        self.fc8 = self.fully_connection(self.fc7, activation=Activation.softmax)
-
+        self.fc6 = self.fully_connection(self.pool5, Activation.relu, 'fc6')
+        self.fc7 = self.fully_connection(self.fc6, Activation.relu, 'fc7')
+        self.fc8 = self.fully_connection(self.fc7, Activation.softmax, 'fc8')
 
         self.net = self.fc8
 
@@ -58,31 +54,32 @@ class Vgg16:
 
 
 
-    def pooling(self, input, name='pool'):
+    def pooling(self, input, name):
         """
         Args: output of just before layer
         Return: max_pooling layer
         """
         return tf.nn.max_pool(input, ksize=vgg.ksize, strides=vgg.pool_strides, padding='SAME', name=name)
 
-    def convolution(self, input, name='conv'):
+    def convolution(self, input, name):
         """
         Args: output of just before layer
         Return: convolution layer
         """
         print('Current input size in convolution layer is: '+str(input.get_shape().as_list()))
         with tf.variable_scope(name):
-            kernel = self.getWeight(vgg.structure[self.lcnt][0])
-            bias = self.getBias(vgg.structure[self.lcnt][1])
+            size = vgg.structure[name]
+            kernel = self.getWeight(size[0])
+            bias = self.getBias(size[1])
             conv = tf.nn.conv2d(input, kernel, strides=vgg.conv_strides, padding='SAME', name=name)
-            self.lcnt += 1
         return tf.nn.relu(tf.add(conv, bias))
 
-    def fully_connection(self, input, name='fc', activation=Activation.relu):
+    def fully_connection(self, input, activation, name):
         """
         Args: output of just before layer
         Return: fully_connected layer
         """
+        size = vgg.structure[name]
         with tf.variable_scope(name):
             shape = input.get_shape().as_list()
             dim = 1
@@ -90,15 +87,14 @@ class Vgg16:
                 dim *= d
             x = tf.reshape(input, [-1, dim])
 
-            weights = self.getWeight([dim, vgg.structure[self.lcnt][0][0]])
-            biases = self.getBias(vgg.structure[self.lcnt][1])
+            weights = self.getWeight([dim, size[0][0]])
+            biases = self.getBias(size[1])
 
             fc = tf.nn.bias_add(tf.matmul(x, weights), biases)
             fc = activation(fc)
 
             print('Input shape is: '+str(shape))
             print('Total nuron count is: '+str(dim))
-            self.lcnt += 1
             
             return fc
 
