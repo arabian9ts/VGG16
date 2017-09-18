3c9ae0d9c161d6235745f1f1409ba2008176fdb1
arabian9ts <nok14.tm59@gmail.com>
2017-09-18 22:36:34 +0900

    add: enumeration of activate-method

---
+++ activation.py
@@ -0,0 +1,17 @@
+"""
+Enumeration class of tensorflow-activation methods
+
+date: 9/18
+author: arabian9ts
+"""
+
+import enum
+import tensorflow as tf
+
+class Activation(enum.Enum):
+    relu = tf.nn.relu
+    softmax = tf.nn.softmax
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+print(sess.run(Activation.relu(1)))
